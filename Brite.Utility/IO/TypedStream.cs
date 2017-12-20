3854bab61089a476c1bd946c2432572eaddf78ce
Eyaz Rehman <scrappycocos@hotmail.com>
2017-12-20 17:03:33 -0500

    Throw exception if no data is available to read bool

--- Brite.Utility/IO/TypedStream.cs
+++ Brite.Utility/IO/TypedStream.cs
@@ -78,7 +78,7 @@ namespace Brite.Utility.IO
         public async Task<int> ReadAsync()
         {
             var b = new byte[1];
-            return await ReadBytesAsync(b, b.Length) <= 0 ? -1 : b[0];
+            return await ReadBytesAsync(b, b.Length) < 0 ? -1 : b[0];
         }
 
         public async Task WriteAsync(byte obj)
@@ -139,7 +139,7 @@ namespace Brite.Utility.IO
 
             var b = await ReadAsync();
             if (b < 0)
-                return false;
+                throw new TimeoutException("Unable to read data");
 
             return b == 1;
         }
